04b47815b754bef43cdd35991ea3f6526394aecc
Afuwape Tunde <afuwapetunde@MacBook-Pro.local>
2025-07-25 11:52:48 +0100

    added the filamentuser contract to enable my user login to the application

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,6 +52,6 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, '@gmail.com');
+        return str_ends_with($this->email, '@afuwapetunde.com');
     }
 }
